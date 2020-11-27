149d7410decec8bd7c47b4a5f722e99be6420bad
Tom Deseyn <tom.deseyn@gmail.com>
2020-11-27 14:33:24 +0100

    Initial

---
+++ src/MyOrg.ClassLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MyOrg.ClassLib
+{
+    public class Class1
+    {
+    }
+}
---
+++ test/MyOrg.ClassLib.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace MyOrg.ClassLib.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
